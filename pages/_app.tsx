8ad90a6cc033aea65756e24f938f8a0c8e84bbed
sw <tansw98@gmail.com>
2021-10-16 18:06:38 +0800

    add theme toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,17 @@
 import type { AppProps } from "next/app";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-import { Menu } from "@mui/icons-material";
+import { Box } from "@mui/material";
+import { ColorModeProvider } from "../lib/contexts/ThemeContext";
+import AppBar from "../lib/components/AppBar";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <Menu />
-        </IconButton>
-        <Typography variant="h6" component="span" sx={{ flexGrow: 1 }}>
-          Next.js Playground
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    <Component {...pageProps} />
+    <ColorModeProvider>
+      <>
+        <AppBar />
+        <Component {...pageProps} />
+      </>
+    </ColorModeProvider>
   </Box>
 );
 export default MyApp;
