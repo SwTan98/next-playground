4bb26bbaf62a55822a8a0040e0ff1d24353d9312
sw <tansw98@gmail.com>
2021-10-16 14:33:29 +0800

    add mui, sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <Menu />
+        </IconButton>
+        <Typography variant="h6" component="span" sx={{ flexGrow: 1 }}>
+          Next.js Playground
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Component {...pageProps} />
+  </Box>
+);
+export default MyApp;
