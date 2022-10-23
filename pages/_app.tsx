3d3865f06f7c9ca9dcede99833e0a6081f0215e8
See Wei <17246235+SwTan98@users.noreply.github.com>
2022-10-23 11:37:48 +0800

    Revert "feat responsive gallery"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,16 @@ import { Box, CssBaseline } from "@mui/material";
 import { ColorModeProvider } from "../lib/contexts/ThemeContext";
 import AppBar from "../components/AppBar";
 import "../styles/globals.css";
-import { ResponsiveProvider } from "../lib/contexts/ResponsiveContext";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ColorModeProvider>
-    <ResponsiveProvider>
-      <>
-        <CssBaseline />
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar />
-          <Component {...pageProps} />
-        </Box>
-      </>
-    </ResponsiveProvider>
+    <>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar />
+        <Component {...pageProps} />
+      </Box>
+    </>
   </ColorModeProvider>
 );
 export default MyApp;
