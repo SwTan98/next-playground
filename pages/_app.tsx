76136f080d93807f407a59e38cd7ed625ae6b270
Jay Dablue <93467663+JayDablue@users.noreply.github.com>
2022-10-23 11:51:09 +0800

    feat responsive masonry columns

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,19 @@ import { Box, CssBaseline } from "@mui/material";
 import { ColorModeProvider } from "../lib/contexts/ThemeContext";
 import AppBar from "../components/AppBar";
 import "../styles/globals.css";
+import { ResponsiveProvider } from "../lib/contexts/ResponsiveContext";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ColorModeProvider>
-    <>
-      <CssBaseline />
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar />
-        <Component {...pageProps} />
-      </Box>
-    </>
+    <ResponsiveProvider>
+      <>
+        <CssBaseline />
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar />
+          <Component {...pageProps} />
+        </Box>
+      </>
+    </ResponsiveProvider>
   </ColorModeProvider>
 );
 export default MyApp;
